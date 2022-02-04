cd8cdb1bcc8b6f4e3a70c482a5370baf0f7b6241
thirdmadman <thirdmadman@gmail.com>
2022-02-04 22:04:23 +0400

    feat: add GlobalConstants

---
+++ src/GlobalConstants.ts
@@ -0,0 +1,24 @@
+class GlobalConstants {
+  public static DEFAULT_API_URL = 'https://thirdmadman-rs-lang.herokuapp.com';
+
+  public static API_ENDPOINT_WORDS = '/words';
+
+  public static API_ENDPOINT_USERS = '/users';
+
+  public static API_ENDPOINT_SIGNIN = '/signin';
+
+  public static ROUTE_MAIN = '/main';
+
+  public static ROUTE_AUTH = '/auth';
+
+  public static ROUTE_WORDBOOK = '/wordbook';
+
+  public static ROUTE_AUDIOCALL = '/audiocall';
+
+  public static ROUTE_SPRINT = '/sprint';
+
+  public static ROUTE_VOCABULARY = '/vocabulary';
+
+  public static ROUTE_STATISTICS = '/statistics';
+}
+export default GlobalConstants;
