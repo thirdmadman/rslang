c9fafb43f957ffbbd90777f8f2f397e27664687b
annettabel87 <annettabel@rambler.ru>
2022-02-10 00:02:01 +0500

    feat: add pagiation

--- src/app/controllers/WordbookController.ts
+++ src/app/controllers/WordbookController.ts
@@ -1,14 +1,20 @@
 import { AbstractController } from './AbstractController';
 import { CardField } from '../views/wordBook/cardsField/CardsField';
 import { WordService } from '../services/WordService';
+// import { IPaginatedArray } from '../interfaces/IPaginatedArray';
+// import { IWord } from '../interfaces/IWord';
+// import { GlobalConstants } from '../../GlobalConstants';
+// import { PathBus } from '../services/PathBus';
 // import { dch } from '../views/dch';
 
 export class WordbookController extends AbstractController {
   resolve(path: string) {
     this.rootNode.innerHTML = `WordbookController - ${path}`;
-
-    WordService.getWordsByGroupAndPage(1, 1).then((data) => {
+    const currentPage = +path.split('/')[2];
+    const currentGroup = +path.split('/')[1];
+    WordService.getWordsByGroupAndPage(currentGroup, currentPage).then((data) => {
       const cardField = new CardField(data);
+      console.log(data);
       this.rootNode.append(cardField.getElement());
     }).catch(() => {});
   }
--- src/app/views/wordBook/cardsField/CardsField.ts
+++ src/app/views/wordBook/cardsField/CardsField.ts
@@ -5,12 +5,31 @@ import Renderable from '../../Renderable';
 // import { WordService } from '../../../services/WordService';
 import { IPaginatedArray } from '../../../interfaces/IPaginatedArray';
 import './CardField.scss';
+import { PathBus } from '../../../services/PathBus';
+import { GlobalConstants } from '../../../../GlobalConstants';
 
 export class CardField extends Renderable {
+  data: IPaginatedArray<IWord>;
+
+  pagenationNum: HTMLElement;
+
+  prevButton: HTMLElement;
+
   constructor(data: IPaginatedArray<IWord>) {
     super();
-    this.rootNode = dch('div', ['cards-container'], '');
-    this.addCards(data.array);
+    this.data = data;
+    this.prevButton = dch('button', ['pagination-button', 'prev'], 'prev');
+    this.prevButton.onclick = () => this.changePage('prev');
+    const nextButton = dch('button', ['pagination-button', 'next'], 'next');
+    nextButton.onclick = () => this.changePage('next');
+    this.pagenationNum = dch(
+      'div',
+      ['pagination-text'],
+      `${this.data.currentPage}/${GlobalConstants.NUMBER_OF_PAGES - 1}`,
+    );
+    const pagenation = dch('div', ['pagination'], '', this.prevButton, this.pagenationNum, nextButton);
+    this.rootNode = dch('div', ['cards-container'], '', pagenation);
+    this.addCards(this.data.array);
   }
 
   addCards(arrayWords: IWord[]) {
@@ -19,4 +38,29 @@ export class CardField extends Renderable {
       this.rootNode.append(card.getElement());
     });
   }
+
+  changePage(btn: string) {
+    if (this.data.currentPage < 1) {
+      this.data.currentPage = 1;
+    } else if (this.data.currentPage > GlobalConstants.NUMBER_OF_PAGES) {
+      this.data.currentPage = GlobalConstants.NUMBER_OF_PAGES;
+    }
+    if (btn === 'prev') {
+      if (this.data.currentPage > 0) this.data.currentPage--;
+      PathBus.setCurrentPath(
+        `${GlobalConstants.ROUTE_WORDBOOK}/${this.data.currentGroup}/${this.data.currentPage}`,
+      );
+    }
+
+    if (btn === 'next') {
+      if (this.data.currentPage < GlobalConstants.NUMBER_OF_PAGES - 1) this.data.currentPage++;
+      this.updateCardField(this.data);
+      PathBus.setCurrentPath(
+        `${GlobalConstants.ROUTE_WORDBOOK}/${this.data.currentGroup}/${this.data.currentPage}`,
+      );
+    }
+    this.pagenationNum.innerHTML = `${this.data.currentPage}/${GlobalConstants.NUMBER_OF_PAGES - 1}`;
+  }
+
+  updateCardField: (data: IPaginatedArray<IWord>) => void = () => {};
 }
