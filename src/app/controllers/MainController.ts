71183060bc4c46ebc96f82b988b74ec8ff0ef48e
annettabel87 <annettabel@rambler.ru>
2022-02-21 00:53:59 +0500

    feat: create menu

--- src/app/controllers/MainController.ts
+++ src/app/controllers/MainController.ts
@@ -1,7 +1,10 @@
 import { AbstractController } from './AbstractController';
+import { Menu } from '../views/menu/Menu';
 
 export class MainController extends AbstractController {
   resolve() {
-    this.rootNode.innerHTML = 'MainController';
+    // this.rootNode.innerHTML = 'MainController';
+    const menu = new Menu();
+    this.rootNode.append(menu.getElement());
   }
 }
---
+++ src/app/views/menu/Menu.ts
@@ -0,0 +1,75 @@
+import { GlobalConstants } from '../../../GlobalConstants';
+// import { PathBus } from '../../services/PathBus';
+import { dch } from '../dch';
+import Renderable from '../Renderable';
+import './Menu.scss';
+
+const menuData = [
+  {
+    title: 'Their memories',
+    path: `${GlobalConstants.ROUTE_WORDBOOK}/1/1`,
+  },
+  {
+    title: 'Audio decoding',
+    path: GlobalConstants.ROUTE_AUDIOCALL,
+  },
+  {
+    title: 'Meaning resolving',
+    path: GlobalConstants.ROUTE_SPRINT,
+  },
+  {
+    title: 'Identity Recognizing',
+    path: GlobalConstants.ROUTE_AUTH,
+  },
+  {
+    title: 'Ancestors',
+    path: GlobalConstants.ROUTE_MAIN,
+  },
+];
+
+export class Menu extends Renderable {
+  appNameContainer: HTMLElement;
+
+  navigationContainer: HTMLElement;
+
+  name: HTMLElement;
+
+  main: HTMLElement;
+
+  nameText: HTMLElement;
+
+  navigation: HTMLElement;
+
+  navigationList: HTMLElement;
+
+  logoContainer: HTMLElement;
+
+  closeBtn: HTMLElement;
+
+  constructor() {
+    super();
+    this.nameText = dch('div', ['app-name--text'], 'Forgotten Words');
+    this.name = dch('div', ['app-name'], '', this.nameText);
+    this.logoContainer = dch('div', ['logo-container']);
+    this.navigationList = dch('ul', ['nav-menu--list']);
+    menuData.forEach((item) => {
+      const link = dch('a', ['nav-menu--link'], `${item.title}`) as HTMLAnchorElement;
+      link.href = `#${item.path}`;
+      const list = dch('li', ['nav-menu--item'], '', link);
+      this.navigationList.append(list);
+    });
+    this.navigation = dch('nav', ['nav-menu'], '', this.navigationList);
+    this.appNameContainer = dch('div', ['app-name-container'], '', this.name);
+    this.navigationContainer = dch('div', ['navigation-container'], '', this.logoContainer, this.navigation);
+    this.closeBtn = dch('button', ['close-button']);
+    this.closeBtn.onclick = () => {
+      this.destroy();
+    };
+    this.main = dch('div', ['main'], '', this.appNameContainer, this.navigationContainer);
+    this.rootNode = dch('div', ['menu'], '', this.main, this.closeBtn);
+  }
+
+  destroy = () => {
+    this.rootNode.remove();
+  };
+}
