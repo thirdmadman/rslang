847d3cbe6814e8b3cd681bc8a23194cd998e7bc6
thirdmadman <thirdmadman@gmail.com>
2022-02-21 23:50:53 +0400

    feat: add path parameters validation

--- src/app/controllers/WordbookController.ts
+++ src/app/controllers/WordbookController.ts
@@ -1,12 +1,26 @@
 import { AbstractController } from './AbstractController';
 import { WordService } from '../services/WordService';
 import { Wordbook } from '../views/wordBook/Wordbook';
+import { GlobalConstants } from '../../GlobalConstants';
 
 export class WordbookController extends AbstractController {
   resolve(path: string) {
     this.rootNode.innerHTML = '';
-    const currentPage = +path.split('/')[2];
-    const currentGroup = +path.split('/')[1];
+    let currentPage = +path.split('/')[2];
+    let currentGroup = +path.split('/')[1];
+
+    if (currentPage < 1) {
+      currentPage = 1;
+    } else if (currentPage > GlobalConstants.NUMBER_OF_PAGES) {
+      currentPage = GlobalConstants.NUMBER_OF_PAGES;
+    }
+
+    if (currentGroup < 1) {
+      currentGroup = 1;
+    } else if (currentGroup > GlobalConstants.NUMBER_OF_GROUP_NO_AUTH_USER) {
+      currentGroup = GlobalConstants.NUMBER_OF_GROUP_NO_AUTH_USER;
+    }
+
     WordService.getWordsByGroupAndPage(currentGroup - 1, currentPage - 1).then((data) => {
       this.rootNode.append(new Wordbook(data).getElement());
     }).catch((e) => console.error(e));
