8bed80c8a635722fc871070d4709ebe3ca425b60
thirdmadman <thirdmadman@gmail.com>
2022-02-21 21:32:57 +0400

    feat: remove pagination from container

--- src/app/views/wordBook/cardsField/CardsField.ts
+++ src/app/views/wordBook/cardsField/CardsField.ts
@@ -4,8 +4,6 @@ import { Card } from './card/Card';
 import Renderable from '../../Renderable';
 import { IPaginatedArray } from '../../../interfaces/IPaginatedArray';
 import './CardsField.scss';
-import { PathBus } from '../../../services/PathBus';
-import { GlobalConstants } from '../../../../GlobalConstants';
 import { TokenProvider } from '../../../services/TokenProvider';
 import { UserWordService } from '../../../services/UserWordService';
 import { IWordAdanced } from '../../../interfaces/IWordAdvanced';
@@ -13,24 +11,10 @@ import { IWordAdanced } from '../../../interfaces/IWordAdvanced';
 export class CardsField extends Renderable {
   data: IPaginatedArray<IWord>;
 
-  pagenationNum: HTMLElement;
-
-  prevButton: HTMLElement;
-
   constructor(data: IPaginatedArray<IWord>) {
     super();
     this.data = data;
-    this.prevButton = dch('button', ['pagination-button', 'prev'], 'prev');
-    this.prevButton.onclick = () => this.changePage('prev');
-    const nextButton = dch('button', ['pagination-button', 'next'], 'next');
-    nextButton.onclick = () => this.changePage('next');
-    this.pagenationNum = dch(
-      'div',
-      ['pagination-text'],
-      `${this.data.currentPage + 1}/${GlobalConstants.NUMBER_OF_PAGES}`,
-    );
-    const pagination = dch('div', ['pagination'], '', this.prevButton, this.pagenationNum, nextButton);
-    this.rootNode = dch('div', ['cards-container'], '', pagination);
+    this.rootNode = dch('div', ['cards-container']);
 
     const userId = TokenProvider.getUserId();
     let advancedWords = this.data.array.map((word) => ({ word } as IWordAdanced));
@@ -60,27 +44,5 @@ export class CardsField extends Renderable {
       const card = new Card(el);
       this.rootNode.append(card.getElement());
     });
-  }
-
-  changePage(btn: string) {
-    if (this.data.currentPage < 0) {
-      this.data.currentPage = 0;
-    } else if (this.data.currentPage > GlobalConstants.NUMBER_OF_PAGES) {
-      this.data.currentPage = GlobalConstants.NUMBER_OF_PAGES;
-    }
-    if (btn === 'prev') {
-      if (this.data.currentPage > 0) this.data.currentPage--;
-      PathBus.setCurrentPath(
-        `${GlobalConstants.ROUTE_WORDBOOK}/${this.data.currentGroup + 1}/${this.data.currentPage + 1}`,
-      );
-    }
-
-    if (btn === 'next') {
-      if (this.data.currentPage < GlobalConstants.NUMBER_OF_PAGES - 1) this.data.currentPage++;
-      PathBus.setCurrentPath(
-        `${GlobalConstants.ROUTE_WORDBOOK}/${this.data.currentGroup + 1}/${this.data.currentPage + 1}`,
-      );
-    }
-    this.pagenationNum.innerHTML = `${this.data.currentPage}/${GlobalConstants.NUMBER_OF_PAGES - 1}`;
   }
 }
