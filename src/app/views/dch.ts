86eac9dca716fd72ea82a360aba95278a50f8439
thirdmadman <thirdmadman@gmail.com>
2022-03-03 14:10:40 +0400

    refactor: fix typo and rename variable

--- src/app/views/dch.ts
+++ src/app/views/dch.ts
@@ -2,20 +2,20 @@
  * DOM Creation Helper
  * @param tagName string
  * @param classList Array<string>
- * @param contetnt string
- * @param childs Array<HTMLElement>
+ * @param content string
+ * @param childNodes Array<HTMLElement>
  * @returns HTMLElement
  */
-export function dch(tagName = 'div', classList = [''], contetnt = '', ...childs: Array<HTMLElement>) {
+export function dch(tagName = 'div', classList = [''], content = '', ...childNodes: Array<HTMLElement>) {
   const node = document.createElement(tagName);
   if (classList.length > 0) {
     node.classList.add(...classList);
   }
-  if (contetnt.length > 0) {
-    node.innerHTML = contetnt;
+  if (content.length > 0) {
+    node.innerHTML = content;
   }
-  if (childs.length > 0) {
-    childs.forEach((el) => node.append(el));
+  if (childNodes.length > 0) {
+    childNodes.forEach((el) => node.append(el));
   }
   return node;
 }
